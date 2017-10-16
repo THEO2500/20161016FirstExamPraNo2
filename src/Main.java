901ba926698658e27a56a2d901a35b75a0f8a200
THEO2500 <worldpeace871219@gmail.com>
2017-10-16 23:09:29 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        float c,f;
+        c=in.nextFloat();
+        f=(c*9/5)+32;
+        System.out.printf("%f",f);
+//sample input2的輸出不符合我覺得是題目的問題，
+    }
+}
